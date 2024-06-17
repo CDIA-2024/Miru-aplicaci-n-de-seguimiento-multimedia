526157e5939f6ddcce5b035d10078e12b4632d5a
maxi09perez <126114458+maxi09perez@users.noreply.github.com>
2024-06-16 23:04:34 -0300

    terminado datos.sql

--- BD/datos.sql
+++ BD/datos.sql
@@ -10,11 +10,17 @@ INSERT INTO desarrollador (nombre) VALUES
 ("Bethesda"),
 ("Niantic");
 INSERT INTO director (nombre) VALUES
+("Justin Benson"),
 ("Tim Burton"),
-("John Ford");
+("Matt Groening"),
+("Andrew Adamson"),
+("Ivan Reitman");
 INSERT INTO productora (nombre) VALUES
 ("Sony Pictures"),
-("Walt Disney");
+("Walt Disney"),
+("21st Century Fox"),
+("DreamWorks"),
+("Columbia Pictures");
 INSERT INTO genero (nombre) VALUES
 ("Comedia"),
 ("Terror"),
@@ -22,7 +28,7 @@ INSERT INTO genero (nombre) VALUES
 ("Shonen");
 INSERT INTO estudioanimacion (nombre) VALUES
 ("Toei"),
-("Pierrot");
+("Pierot");
 INSERT INTO contenido (titulo, anio_lanzamiento, id_genero) VALUES
 ("Shrek", 2001, 1),
 ("Cazafantasmas", 1984, 2),
@@ -32,3 +38,24 @@ INSERT INTO contenido (titulo, anio_lanzamiento, id_genero) VALUES
 ("Los Simpsons", 1989, 1),
 ("Bleach", 2004, 4),
 ("Dragon Ball", 1984, 4);
+INSERT INTO lista (fecha_creacion, id_usuario, id_contenido, nombre) VALUES
+("2024-06-13", 1, 4, "maxlist"),
+("2023-07-01", 2, 1, "mi_list");
+INSERT INTO videojuego (id_contenido, plataforma, id_desarrolador) VALUES
+(3, "pc", 1),
+(4, "game boy", 2);
+INSERT INTO anime (id_contenido, capitulos, id_estudio, id_estado) VALUES
+(7, 392, 2, 2),
+(8, 600, 1, 2);
+INSERT INTO serie (titulo, id_contenido, temporadas, id_director, id_productora, id_estado) VALUES
+("Loki", 5, 2, 1, 2, 1),
+("Los Simpsons", 6, 35, 3, 3, 2);
+INSERT INTO pelicula (id_contenido, duracion, id_director, id_productora) VALUES
+(1, 89, 4, 4),
+(2, 75, 5, 5);
+INSERT INTO registro (puntuacion, id_estado, id_usuario, id_contenido) VALUES
+(7.5, 2, 1, 6),
+(9.0, 2, 2, 4);
+INSERT INTO reseña (fecha_creacion, texto, id_usuario, id_contenido, id_registro) VALUES
+("2024-06-16", "Las primeras temporadas son magnificas, las demas con el pasar de tiempo perdieron du magia.", 1, 6, 1),
+("2020-12-28", "Un juego muy divertido y mucho mas facil de comprender que otros rpg.", 2, 4, 2);
